b4efd986561ff023d14be6c68cd489448f397ef7
Niklas Kochdumper <niklas.kochdumper@tum.de>
2023-04-05 16:53:39 -0400

    measure computation time for the scenearios

--- runAllScenarios.py
+++ runAllScenarios.py
@@ -8,13 +8,17 @@ import os
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import time
 
 from vehicle.vehicleParameter import vehicleParameter
+from auxiliary.createVideo import createVideo
 from src.highLevelPlanner import highLevelPlanner
 from src.lowLevelPlannerManeuverAutomaton import lowLevelPlannerManeuverAutomaton
 
 import warnings
 warnings.filterwarnings("ignore")
+
+VIDEO = False           # create videos for all scenarios that can be solved
 
 # parameter for the car
 param = vehicleParameter()
@@ -22,6 +26,7 @@ param = vehicleParameter()
 # get all available CommonRoad scenarios
 path = 'scenarios'
 files = [f for f in listdir(path) if isfile(join(path, f))]
+data = []
 
 # loop over all scenarios
 for f in files:
@@ -31,10 +36,24 @@ for f in files:
 
     # run the motion planner
     try:
+        start_time = time.time()
         plan, vel, space, ref_traj = highLevelPlanner(scenario, planning_problem, param)
         x, u = lowLevelPlannerManeuverAutomaton(scenario, planning_problem, param, plan, vel, space, ref_traj)
+        comp_time = time.time() - start_time
         if x is None:
             print(f + ': failed')
-        #print(f + ': success')
+        else:
+            final_time = x.shape[1] * param['time_step']
+            comp_time = comp_time / final_time
+            data.append([f, comp_time])
+            if VIDEO:
+                createVideo(f, scenario, planning_problem, param, x)
     except:
         print(f + ': failed')
+
+# save computation time in .csv file
+np.savetxt('computation_time.csv', np.asarray(data), delimiter=",", fmt="%s")
+
+# display results
+print(' ')
+print('success rate: ' + str(len(data)/len(files) * 100) + "%")
